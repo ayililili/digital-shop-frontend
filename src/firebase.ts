fc0df76f92685c924f7e40412918c2501fd81391
Ayi <134775797+ayililili@users.noreply.github.com>
2025-06-05 13:46:54 +0800

    refactor: use env vars for firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKV2tzRPxxQBwGZ_vFsyCBMGLhWwAKuYs",
-  authDomain: "test-6bdc5.firebaseapp.com",
-  projectId: "test-6bdc5",
-  storageBucket: "test-6bdc5.firebasestorage.app",
-  messagingSenderId: "581861896209",
-  appId: "1:581861896209:web:0286953c22390a43fcc1b0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
