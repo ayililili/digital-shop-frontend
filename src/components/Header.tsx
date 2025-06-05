e6238cc9d2ea886d83ab7cf1e101367ce514fa83
Ayi <134775797+ayililili@users.noreply.github.com>
2025-06-05 15:21:21 +0800

    Add global header and parallax landing

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/product">Product</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
